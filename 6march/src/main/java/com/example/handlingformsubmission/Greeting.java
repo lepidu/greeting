413897f12256f439a8fbc321d43d88d17fdacd8d
LEISLY PINO <55885941+lepidu@users.noreply.github.com>
2023-03-06 14:13:40 +0000

    Add files via upload

---
+++ 6march/src/main/java/com/example/handlingformsubmission/Greeting.java
@@ -0,0 +1,56 @@
+package com.example.handlingformsubmission;
+
+public class Greeting {
+	  private String name;
+	  private String address;
+	  private String phone;
+	  private String date;
+	  private String find_us;
+	  private String msg;
+
+	  public String getName() {
+	    return name;
+	  }
+
+	  public void setName(String name) {
+	    this.name = name;
+	  }
+
+	  public String getAddress() {
+	    return address;
+	  }
+
+	  public void setAddress(String address) {
+	    this.address = address;
+	  }
+	  public String getPhone() {
+		    return phone;
+		  }
+
+		  public void setPhone(String phone) {
+		    this.phone = phone;
+		  }
+
+		  public String getDate() {
+		    return date;
+		  }
+
+		  public void setDate(String date) {
+		    this.date = date;
+		  }	  
+		  public String getFind() {
+			    return find_us;
+			  }
+
+			  public void setFind(String find_us) {
+			    this.find_us = find_us;
+			  }
+
+			  public String getMsg() {
+			    return msg;
+			  }
+
+			  public void setMsg(String msg) {
+			    this.msg = msg;
+			  }
+}
